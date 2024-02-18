6275e09fa44b11af64a1cb539e814a974991bd4c
Bumpist <104641096+parksubeom@users.noreply.github.com>
2024-02-18 17:43:25 +0900

    refactor : 리드미페이지 100vh => 100%  /  뒤로가기 버튼컴포넌트 추가

--- src/components/project/anischool/Readme.tsx
+++ src/components/project/anischool/Readme.tsx
@@ -1,126 +1,142 @@
 // ProjectInfo.tsx
 import React from 'react';
 import styled from 'styled-components';
-
+import Button from '../../../common/Button';
+import { useNavigate } from 'react-router-dom';
 const Readme: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <MainTitleSection>
-        <MainTitle>프로필 이미지 생성 서비스 [애니스쿨]</MainTitle>
-      </MainTitleSection>
-      <Container>
-        <Section>
-          <Title>📌Summary</Title>
-          <Content>
-            <p>
-              <strong>
-                자신의 개성이 중요해진 요즘, 본인의 프로필을 업로드 해야 하지만 얼굴을 올리기 싫은 사람들을 위해 귀여운
-                동물 이미지를 만들어주는 서비스입니다.
-              </strong>
-            </p>
-            <p>
-              다양한 이미지 레이어를 겹쳐서 하나의 이미지를 만들고, 각 파츠 이미지 레이어마다 레어한 확률을 설정하여
-              뽑기의 재미를 더했습니다.
-            </p>
-            <p></p>
-            <h2>❗주요 기능</h2>
-            <li>랜덤한 캐릭터 이미지 생성</li>
-            <li>파츠별 이미지 변경</li>
-            <li>랜덤버튼 클릭 시, 일정확률로 레어요소 등장</li>
-            <li>프로필이미지 png파일로 내려받기</li>
-          </Content>
-        </Section>
-        <Section>
-          <Title>🤔Background</Title>
-          <Content>
-            {' '}
-            <p>부트캠프를 진행 할 당시 주로 디스코드를 통해 커뮤니케이션을 하고, 협업을 진행했었습니다.</p>
-            <p>
-              디스코드는 프로필사진을 업로드 가능했는데, 몇몇 동기들끼리 비슷한 프로필 아이콘을 설정해놓을 걸
-              발견했습니다.
-            </p>
-            <p>
-              이는 그들간의 친분을 나타낸다고 생각했고, 다르게 생각하면 이미 저들만의 무리가 형성됐다고 볼 수도
-              있었습니다. 특히나 협업을 주로 하게 될 부트캠프에서 여러 무리로 나뉘다보면 소통의 기회가 적어질것이라는
-              고민이 있었습니다.
-            </p>
-            <p>
-              이러한 고민을 해결하기위해 같은 코스 동기들끼리 다같이 사용가능한 프로필이미지 생성기를 만들어서
-              공유한다면, 무리로 나뉘는게아닌 같은 기수 동기끼리 소속감을 가지지 않을까라는 생각에 개발을 마음먹게
-              되었습니다.
-            </p>
-          </Content>
-        </Section>
-        <Section>
-          <Title>🔨Technology Stack</Title>
-          <Content>
-            {' '}
-            <li>개발환경 : React / JavaScript</li>
-            <li>상태관리 : Recoil</li>
-            <li>스타일 : 외부 스타일시트</li>
-            <li>배포 : AWS S3 / AWS CloudFront</li>
-            <li>데이터 패칭 및 캐싱 : X </li>
-          </Content>
-        </Section>
-        <Section>
-          <Title>🔍What I learned</Title>
-          <Content>
-            <h2>랜덤 이미지 생성</h2>
-            <li>
-              랜덤버튼 클릭 시, (Math.random() * 해당파츠의 종류 갯수) 의 결과값을 상태에 저장하고, 해당 상태를 이미지를
-              보여주는 컴포넌트에 props로 넘겨줍니다. 랜덤버튼 컴포넌트에서 해당 상태를 전달받으면 해당 파츠의 이미지를
-              폴더에서 상태와 같은 숫자의 이미지를 불러오는 식으로 구현했습니다.
-            </li>
-            <li>
-              리액트를 입문하고 처음 만들어 본 프로젝트인 만큼, 리액트의 데이터 흐름과 상태관리의 중요성 나아가
-              전역상태관리가 필요하다고 느꼈고, 리코일을 통해 전역에서 상태를 관리하는식으로 리팩토링 했습니다.
-            </li>
-            <h2>완성된 이미지 png로 내보내기</h2>
-            <li>
-              dom-to-image 라는 이미지 캡처 라이브러리와 캡처한 파일을 디바이스에 저장해주는 file-server라는
-              라이브러리를 사용했습니다.
-            </li>
-            <li>
-              이 과정에서 DOM 을 조작해야 할 일이 생겼는데, querySelector를 통해 돔을 조작하는것 보다 useRef를
-              사용하는게 리액트의 선언형 프로그래밍에 더 부합한다는 걸 배웠고, 가급적 DOM 조작은 지양해야한다는 걸
-              알게됐습니다.
-            </li>
-            <h2>라이트하우스 최적화</h2>
-            <li>
-              프로젝트를 마무리하고 라이트 하우스를 통해 성능을 체크 했을 땐, SEO 를 제외한 모든 부분의 성능이 기대보다
-              낮았습니다.
-            </li>
-            <li>
-              이를 최적화 하기 위해 크게 두가지의 개선방안을 생각했습니다. 첫번째로 코드에서의 최적화, 두번째로 리소스의
-              최적화 이렇게 두가지를 생각했습니다.
-            </li>
-            <li>
-              코드에서의 최적화는 이미지나 버튼에 aria-label와 alt 속성을 통해 웹 접근성을 높이고, Lazy Loding 을 통한
-              이미지 페인팅 최적화를 시도했습니다.
-            </li>
-            <li>
-              리소스 최적화로는 png파일이던 그림소스들을 webp로 변환하여 용량을 압축했고, 트리셰이킹을 통해 미사용
-              모듈들을 제거함으로써 퍼포먼스를 증가시켰습니다.
-            </li>
-            <li>
-              빠른 페이지 로딩, 접근성 등이 사용자 만족도를 얼마나 개선시키고 나아가 검색 엔진 최적화에도 긍정적인
-              영향을 끼친다는 걸 느꼈습니다.
-            </li>
-          </Content>
-        </Section>
-      </Container>
+      <WraperContainer>
+        <MainTitleSection>
+          <MainTitle>프로필 이미지 생성 서비스 [애니스쿨]</MainTitle>
+        </MainTitleSection>
+        <Button onClick={() => navigate(-1)} $width="10%" $bgcolor="gray" $color="white">
+          뒤로가기
+        </Button>
+        <Container>
+          <Section>
+            <Title>📌Summary</Title>
+            <Content>
+              <p>
+                <strong>
+                  자신의 개성이 중요해진 요즘, 본인의 프로필을 업로드 해야 하지만 얼굴을 올리기 싫은 사람들을 위해
+                  귀여운 동물 이미지를 만들어주는 서비스입니다.
+                </strong>
+              </p>
+              <p>
+                다양한 이미지 레이어를 겹쳐서 하나의 이미지를 만들고, 각 파츠 이미지 레이어마다 레어한 확률을 설정하여
+                뽑기의 재미를 더했습니다.
+              </p>
+              <p></p>
+              <h2>❗주요 기능</h2>
+              <li>랜덤한 캐릭터 이미지 생성</li>
+              <li>파츠별 이미지 변경</li>
+              <li>랜덤버튼 클릭 시, 일정확률로 레어요소 등장</li>
+              <li>프로필이미지 png파일로 내려받기</li>
+            </Content>
+          </Section>
+          <Section>
+            <Title>🤔Background</Title>
+            <Content>
+              {' '}
+              <p>부트캠프를 진행 할 당시 주로 디스코드를 통해 커뮤니케이션을 하고, 협업을 진행했었습니다.</p>
+              <p>
+                디스코드는 프로필사진을 업로드 가능했는데, 몇몇 동기들끼리 비슷한 프로필 아이콘을 설정해놓을 걸
+                발견했습니다.
+              </p>
+              <p>
+                이는 그들간의 친분을 나타낸다고 생각했고, 다르게 생각하면 이미 저들만의 무리가 형성됐다고 볼 수도
+                있었습니다. 특히나 협업을 주로 하게 될 부트캠프에서 여러 무리로 나뉘다보면 소통의 기회가 적어질것이라는
+                고민이 있었습니다.
+              </p>
+              <p>
+                이러한 고민을 해결하기위해 같은 코스 동기들끼리 다같이 사용가능한 프로필이미지 생성기를 만들어서
+                공유한다면, 무리로 나뉘는게아닌 같은 기수 동기끼리 소속감을 가지지 않을까라는 생각에 개발을 마음먹게
+                되었습니다.
+              </p>
+            </Content>
+          </Section>
+          <Section>
+            <Title>🔨Technology Stack</Title>
+            <Content>
+              {' '}
+              <li>개발환경 : React / JavaScript</li>
+              <li>상태관리 : Recoil</li>
+              <li>스타일 : 외부 스타일시트</li>
+              <li>배포 : AWS S3 / AWS CloudFront</li>
+              <li>데이터 패칭 및 캐싱 : X </li>
+            </Content>
+          </Section>
+          <Section>
+            <Title>🔍What I learned</Title>
+            <Content>
+              <h2>랜덤 이미지 생성</h2>
+              <li>
+                랜덤버튼 클릭 시, (Math.random() * 해당파츠의 종류 갯수) 의 결과값을 상태에 저장하고, 해당 상태를
+                이미지를 보여주는 컴포넌트에 props로 넘겨줍니다. 랜덤버튼 컴포넌트에서 해당 상태를 전달받으면 해당
+                파츠의 이미지를 폴더에서 상태와 같은 숫자의 이미지를 불러오는 식으로 구현했습니다.
+              </li>
+              <li>
+                리액트를 입문하고 처음 만들어 본 프로젝트인 만큼, 리액트의 데이터 흐름과 상태관리의 중요성 나아가
+                전역상태관리가 필요하다고 느꼈고, 리코일을 통해 전역에서 상태를 관리하는식으로 리팩토링 했습니다.
+              </li>
+              <h2>완성된 이미지 png로 내보내기</h2>
+              <li>
+                dom-to-image 라는 이미지 캡처 라이브러리와 캡처한 파일을 디바이스에 저장해주는 file-server라는
+                라이브러리를 사용했습니다.
+              </li>
+              <li>
+                이 과정에서 DOM 을 조작해야 할 일이 생겼는데, querySelector를 통해 돔을 조작하는것 보다 useRef를
+                사용하는게 리액트의 선언형 프로그래밍에 더 부합한다는 걸 배웠고, 가급적 DOM 조작은 지양해야한다는 걸
+                알게됐습니다.
+              </li>
+              <h2>라이트하우스 최적화</h2>
+              <li>
+                프로젝트를 마무리하고 라이트 하우스를 통해 성능을 체크 했을 땐, SEO 를 제외한 모든 부분의 성능이
+                기대보다 낮았습니다.
+              </li>
+              <li>
+                이를 최적화 하기 위해 크게 두가지의 개선방안을 생각했습니다. 첫번째로 코드에서의 최적화, 두번째로
+                리소스의 최적화 이렇게 두가지를 생각했습니다.
+              </li>
+              <li>
+                코드에서의 최적화는 이미지나 버튼에 aria-label와 alt 속성을 통해 웹 접근성을 높이고, Lazy Loding 을 통한
+                이미지 페인팅 최적화를 시도했습니다.
+              </li>
+              <li>
+                리소스 최적화로는 png파일이던 그림소스들을 webp로 변환하여 용량을 압축했고, 트리셰이킹을 통해 미사용
+                모듈들을 제거함으로써 퍼포먼스를 증가시켰습니다.
+              </li>
+              <li>
+                빠른 페이지 로딩, 접근성 등이 사용자 만족도를 얼마나 개선시키고 나아가 검색 엔진 최적화에도 긍정적인
+                영향을 끼친다는 걸 느꼈습니다.
+              </li>
+            </Content>
+          </Section>
+        </Container>
+      </WraperContainer>
     </>
   );
 };
 
 export default Readme;
 
+const WraperContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  > button {
+    z-index: 5;
+  }
+`;
+
 // 스타일드 컴포넌트
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   padding: 20px;
   overflow-y: auto; /* 스크롤이 필요한 경우 추가 */
 `;
--- src/components/project/uncover/Readme.tsx
+++ src/components/project/uncover/Readme.tsx
@@ -1,115 +1,131 @@
 // ProjectInfo.tsx
 import React from 'react';
 import styled from 'styled-components';
+import Button from '../../../common/Button';
+import { useNavigate } from 'react-router-dom';
 
 const Readme: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <MainTitleSection>
-        <MainTitle>노 카피라이트 음원 스트리밍 서비스 [UNCOVER]</MainTitle>
-      </MainTitleSection>
-      <Container>
-        <Section>
-          <Title>📌Summary</Title>
-          <Content>
-            <p>
-              <strong>
-                저작권이 없는 노카피라이트 음원을 스트리밍 할 수 있으며,자신만의 플레이리스트를 만들어서 다른사람들과
-                공유할 수 있는 스트리밍 서비스입니다.
-              </strong>
-            </p>
-            <p>
-              이론만 들어 감이 오지않던 타입스크립트를 직접 도입해 본 첫 프로젝트이며, 이외에도 전역상태관리, OAuth 2.0
-              등의 개념을 한 번 더 깊게 다질 수 있었습니다.
-            </p>
-            <p>
-              스타일드 컴포넌트를 이용하여 재사용성 높은 스타일드컴포넌트를 설계해 볼 수 있었고, 동적 스타일링 및 조건부
-              스타일링을 경험하면서 좋은 UI/UX에 대해 고민하는 계기도 되었습니다.
-            </p>
-            <p>
-              어떤 기능을 구현하냐보다, 사용자의 사용성에 집중해보며 유저친화적인 서비스를 설계하기위해 고민할 수 있었던
-              좋은 기회였다고 생각합니다.
-            </p>
-            <h2>❗주요 기능</h2>
-            <li>음원 스트리밍</li>
-            <li>음원 다운로드</li>
-            <li>음원 태그에 따라 분류 및 서칭</li>
-            <li>플레이리스트 CRUD</li>
-            <li>플레이리스트 댓글 CRUD</li>
-            <li>음원이 영상에 어울리는지 핏을 확인하는 믹싱페이지</li>
-          </Content>
-        </Section>
-        <Section>
-          <Title>🤔Background</Title>
-          <Content>
-            {' '}
-            <p>
-              저는 취미로 방송 스트리밍과 유튜브 편집을 했었습니다. 유튜브 영상은 저작권 있는 음원을 추가하면 수익을 낼
-              수 없었고, 그렇기에 저작권 없는 음원들을 일일이 찾으러다녀야 했고 그 음원이 내 영상 분위기에 맞는지
-              하나하나 비교해야하는 수고를 겪어야했습니다.
-            </p>
-            <p>
-              저는 그럴 때마다 어딘가에 저작권없이 편하게 사용가능한 음원들이 모여있고, 그 자리에서 영상과 음원의 핏까지
-              비교해볼 수 있는 서비스가 있다면 좋겠다고 생각했습니다. 오직 영상을 위해서가 아니라도 대부분의 노
-              카피라이트 음원은 가사가 없는 멜로디 음원인 경우가 많은데, 이러한 음원들은 업무를 할 때나 자기전
-              수면음악으로 사용하기에 아주 적절해 보였습니다.
-            </p>
-            <p>
-              음원 스트리밍기능을 추가해야겠다 생각했고, 나아가 자신만의 테마를 가진 플레이리스트를 만들어 서로 공유까지
-              되게 한다면 단순히 음원 다운로드 서비스가 아닌 괜찮은 음원 스트리밍 서비스가 될것이라 생각했기에 이
-              프로젝트를 시작하게 됐습니다.
-            </p>
-          </Content>
-        </Section>
-        <Section>
-          <Title>🔨Technology Stack</Title>
-          <Content>
-            {' '}
-            <li>개발환경 : React / TypeScript </li>
-            <li>상태관리 : Recoil</li>
-            <li>스타일 : Styled-components</li>
-            <li>배포 : AWS EC2 / AWS CloudFront</li>
-            <li>데이터 패칭 및 캐싱 : axios / React-query</li>
-          </Content>
-        </Section>
-        <Section>
-          <Title>🔍What I learned</Title>
-          <Content>
-            <h2>Oauth2로그인 및 로컬 로그인기능 구현 </h2>
-            <li>
-              그동안 수 없이 해온 로그인 인증의 플로우를자세히 알아볼 수 있는 기회였습니다. 또한 프론트엔드와 백엔드
-              간의 통합을 위해 API통신, 토큰 교환 등의 작업을 수행함으로써 클라이언트와 서버 간의 협업 방식을 익힐 수
-              있었습니다. 클라이언트에서 외부 플래폼으로 인가코드를 요청하고, 그에대한 응답을 다시 서버로 보내며 수 많은
-              응답코드에 대한 지식을 익힐 수 있었습니다.
-            </li>
-            <h2>믹싱 페이지 구현</h2>
-            <li>
-              음원과 영상의 핏이 적합한지 대조해 볼 수 있는 믹싱페이지 구현을 맡았습니다. 해당 페이지 구현을 위해서는
-              비디오와 오디오를 동시에 조작해야했고, 그러기 위해선 DOM 조작이 필요했습니다. 이전 토이프로젝트에서는 쿼리
-              셀렉터를 통해 DOM을 조작했지만, 리액트 공식문서를 살펴보니 리액트는 선언형 프로그래밍을 권장하는데 쿼리
-              셀렉터는 명령형 프로그래밍에 더 가깝기 때문에 useRef를 사용하여 이러한 고민을 해결했습니다.
-            </li>
-            <h2>반응형 웹 레이아웃 구현</h2>
-            <li>
-              미디어 쿼리를 활용하여 다양한 화면 크기와 해상도에 대응해볼 수 있었습니다. 또한 Flexbox 를 이용하여
-              요소들을 효율적으로 정렬하고 배치하는 방법을 배웠습니다. 이전까지는 크기단위는 px만 사용해봤었는데 반응형
-              디자인을 위해 rem,em,% 등 여러가지 단위를 이해하고 사용해볼 수 있었습니다.
-            </li>
-          </Content>
-        </Section>
-      </Container>
+      <WraperContainer>
+        <MainTitleSection>
+          <MainTitle>노 카피라이트 음원 스트리밍 서비스 [UNCOVER]</MainTitle>
+        </MainTitleSection>
+        <Button onClick={() => navigate(-1)} $width="10%" $bgcolor="gray" $color="white">
+          뒤로가기
+        </Button>
+        <Container>
+          <Section>
+            <Title>📌Summary</Title>
+            <Content>
+              <p>
+                <strong>
+                  저작권이 없는 노카피라이트 음원을 스트리밍 할 수 있으며,자신만의 플레이리스트를 만들어서 다른사람들과
+                  공유할 수 있는 스트리밍 서비스입니다.
+                </strong>
+              </p>
+              <p>
+                이론만 들어 감이 오지않던 타입스크립트를 직접 도입해 본 첫 프로젝트이며, 이외에도 전역상태관리, OAuth
+                2.0 등의 개념을 한 번 더 깊게 다질 수 있었습니다.
+              </p>
+              <p>
+                스타일드 컴포넌트를 이용하여 재사용성 높은 스타일드컴포넌트를 설계해 볼 수 있었고, 동적 스타일링 및
+                조건부 스타일링을 경험하면서 좋은 UI/UX에 대해 고민하는 계기도 되었습니다.
+              </p>
+              <p>
+                어떤 기능을 구현하냐보다, 사용자의 사용성에 집중해보며 유저친화적인 서비스를 설계하기위해 고민할 수
+                있었던 좋은 기회였다고 생각합니다.
+              </p>
+              <h2>❗주요 기능</h2>
+              <li>음원 스트리밍</li>
+              <li>음원 다운로드</li>
+              <li>음원 태그에 따라 분류 및 서칭</li>
+              <li>플레이리스트 CRUD</li>
+              <li>플레이리스트 댓글 CRUD</li>
+              <li>음원이 영상에 어울리는지 핏을 확인하는 믹싱페이지</li>
+            </Content>
+          </Section>
+          <Section>
+            <Title>🤔Background</Title>
+            <Content>
+              {' '}
+              <p>
+                저는 취미로 방송 스트리밍과 유튜브 편집을 했었습니다. 유튜브 영상은 저작권 있는 음원을 추가하면 수익을
+                낼 수 없었고, 그렇기에 저작권 없는 음원들을 일일이 찾으러다녀야 했고 그 음원이 내 영상 분위기에 맞는지
+                하나하나 비교해야하는 수고를 겪어야했습니다.
+              </p>
+              <p>
+                저는 그럴 때마다 어딘가에 저작권없이 편하게 사용가능한 음원들이 모여있고, 그 자리에서 영상과 음원의
+                핏까지 비교해볼 수 있는 서비스가 있다면 좋겠다고 생각했습니다. 오직 영상을 위해서가 아니라도 대부분의 노
+                카피라이트 음원은 가사가 없는 멜로디 음원인 경우가 많은데, 이러한 음원들은 업무를 할 때나 자기전
+                수면음악으로 사용하기에 아주 적절해 보였습니다.
+              </p>
+              <p>
+                음원 스트리밍기능을 추가해야겠다 생각했고, 나아가 자신만의 테마를 가진 플레이리스트를 만들어 서로
+                공유까지 되게 한다면 단순히 음원 다운로드 서비스가 아닌 괜찮은 음원 스트리밍 서비스가 될것이라
+                생각했기에 이 프로젝트를 시작하게 됐습니다.
+              </p>
+            </Content>
+          </Section>
+          <Section>
+            <Title>🔨Technology Stack</Title>
+            <Content>
+              {' '}
+              <li>개발환경 : React / TypeScript </li>
+              <li>상태관리 : Recoil</li>
+              <li>스타일 : Styled-components</li>
+              <li>배포 : AWS EC2 / AWS CloudFront</li>
+              <li>데이터 패칭 및 캐싱 : axios / React-query</li>
+            </Content>
+          </Section>
+          <Section>
+            <Title>🔍What I learned</Title>
+            <Content>
+              <h2>Oauth2로그인 및 로컬 로그인기능 구현 </h2>
+              <li>
+                그동안 수 없이 해온 로그인 인증의 플로우를자세히 알아볼 수 있는 기회였습니다. 또한 프론트엔드와 백엔드
+                간의 통합을 위해 API통신, 토큰 교환 등의 작업을 수행함으로써 클라이언트와 서버 간의 협업 방식을 익힐 수
+                있었습니다. 클라이언트에서 외부 플래폼으로 인가코드를 요청하고, 그에대한 응답을 다시 서버로 보내며 수
+                많은 응답코드에 대한 지식을 익힐 수 있었습니다.
+              </li>
+              <h2>믹싱 페이지 구현</h2>
+              <li>
+                음원과 영상의 핏이 적합한지 대조해 볼 수 있는 믹싱페이지 구현을 맡았습니다. 해당 페이지 구현을 위해서는
+                비디오와 오디오를 동시에 조작해야했고, 그러기 위해선 DOM 조작이 필요했습니다. 이전 토이프로젝트에서는
+                쿼리 셀렉터를 통해 DOM을 조작했지만, 리액트 공식문서를 살펴보니 리액트는 선언형 프로그래밍을 권장하는데
+                쿼리 셀렉터는 명령형 프로그래밍에 더 가깝기 때문에 useRef를 사용하여 이러한 고민을 해결했습니다.
+              </li>
+              <h2>반응형 웹 레이아웃 구현</h2>
+              <li>
+                미디어 쿼리를 활용하여 다양한 화면 크기와 해상도에 대응해볼 수 있었습니다. 또한 Flexbox 를 이용하여
+                요소들을 효율적으로 정렬하고 배치하는 방법을 배웠습니다. 이전까지는 크기단위는 px만 사용해봤었는데
+                반응형 디자인을 위해 rem,em,% 등 여러가지 단위를 이해하고 사용해볼 수 있었습니다.
+              </li>
+            </Content>
+          </Section>
+        </Container>
+      </WraperContainer>
     </>
   );
 };
 
 export default Readme;
+const WraperContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  > button {
+    z-index: 5;
+  }
+`;
 
 // 스타일드 컴포넌트
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   padding: 20px;
   overflow-y: auto; /* 스크롤이 필요한 경우 추가 */
 `;
